2b16a311f9487bab432e391e87d5e6d1e5ae7ff4
WaghAshu03 <waghashutosh03@gmail.com>
2023-11-14 20:19:07 +0530

    Commit: - Added Pawn Promotion - Calculating current score based on onboard pieces, instead of captured pieces

--- main.c
+++ main.c
@@ -70,6 +70,7 @@ typedef struct piece
     char type;
     char color;
     char *symbol;
+    int piece_active;
 } piece;
 
 typedef struct square
@@ -91,57 +92,180 @@ typedef struct piece_side
 } piece_side;
 
 piece_side white = {
-    .King = {.cost = 900, .type = 'K', .color = 'W', .symbol = "♚"},
-    .Queen = {.cost = 9, .type = 'Q', .color = 'W', .symbol = "♛"},
-    .Knight = {{.cost = 3, .type = 'N', .color = 'W', .symbol = "♞"}, {.cost = 3, .type = 'N', .color = 'W', .symbol = "♞"}},
-    .Bishop = {{.cost = 3, .type = 'B', .color = 'W', .symbol = "♝"}, {.cost = 3, .type = 'B', .color = 'W', .symbol = "♝"}},
-    .Rook = {{.cost = 5, .type = 'R', .color = 'W', .symbol = "♜"}, {.cost = 5, .type = 'R', .color = 'W', .symbol = "♜"}},
+    .King = {.cost = 900, .type = 'K', .color = 'W', .symbol = "♚", .piece_active = 1},
+    .Queen = {.cost = 9, .type = 'Q', .color = 'W', .symbol = "♛", .piece_active = 1},
+    .Knight = {{.cost = 3, .type = 'N', .color = 'W', .symbol = "♞", .piece_active = 1}, {.cost = 3, .type = 'N', .color = 'W', .symbol = "♞", .piece_active = 1}},
+    .Bishop = {{.cost = 3, .type = 'B', .color = 'W', .symbol = "♝", .piece_active = 1}, {.cost = 3, .type = 'B', .color = 'W', .symbol = "♝", .piece_active = 1}},
+    .Rook = {{.cost = 5, .type = 'R', .color = 'W', .symbol = "♜", .piece_active = 1}, {.cost = 5, .type = 'R', .color = 'W', .symbol = "♜", .piece_active = 1}},
     .Pawn = {
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
-        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟"},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
+        {.cost = 1, .type = 'P', .color = 'W', .symbol = "♟", .piece_active = 1},
     }};
 
 piece_side black = {
-    .King = {.cost = -900, .type = 'k', .color = 'B', .symbol = "♔"},
-    .Queen = {.cost = -9, .type = 'q', .color = 'B', .symbol = "♕"},
-    .Knight = {{.cost = -3, .type = 'n', .color = 'B', .symbol = "♘"}, {.cost = -3, .type = 'n', .color = 'B', .symbol = "♘"}},
-    .Bishop = {{.cost = -3, .type = 'b', .color = 'B', .symbol = "♗"}, {.cost = -3, .type = 'b', .color = 'B', .symbol = "♗"}},
-    .Rook = {{.cost = -5, .type = 'r', .color = 'B', .symbol = "♖"}, {.cost = -5, .type = 'r', .color = 'B', .symbol = "♖"}},
+    .King = {.cost = -900, .type = 'k', .color = 'B', .symbol = "♔", .piece_active = 1},
+    .Queen = {.cost = -9, .type = 'q', .color = 'B', .symbol = "♕", .piece_active = 1},
+    .Knight = {{.cost = -3, .type = 'n', .color = 'B', .symbol = "♘", .piece_active = 1}, {.cost = -3, .type = 'n', .color = 'B', .symbol = "♘", .piece_active = 1}},
+    .Bishop = {{.cost = -3, .type = 'b', .color = 'B', .symbol = "♗", .piece_active = 1}, {.cost = -3, .type = 'b', .color = 'B', .symbol = "♗", .piece_active = 1}},
+    .Rook = {{.cost = -5, .type = 'r', .color = 'B', .symbol = "♖", .piece_active = 1}, {.cost = -5, .type = 'r', .color = 'B', .symbol = "♖", .piece_active = 1}},
     .Pawn = {
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
-        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙"},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
+        {.cost = -1, .type = 'p', .color = 'B', .symbol = "♙", .piece_active = 1},
     }};
 
-piece *captured_pieces[size * 4] = {NULL};
-
-// todo: Calculate cost based on onboard pieces not by captured pieces
 int calc_captured_pieces_cost()
 {
     int total = 0;
-    for (int i = 0; i < size * 4; i++)
-    {
-        if (captured_pieces[i] == NULL)
-            break;
-        total +=
-            captured_pieces[i]->cost;
-    }
+
+    // White Pieces
+    if (white.King.piece_active)
+        total += white.King.cost;
+
+    if (white.Queen.piece_active)
+        total += white.Queen.cost;
+
+    if (white.Knight[0].piece_active)
+        total += white.Knight[0].cost;
+
+    if (white.Knight[1].piece_active)
+        total += white.Knight[1].cost;
+
+    if (white.Bishop[0].piece_active)
+        total += white.Bishop[0].cost;
+
+    if (white.Bishop[1].piece_active)
+        total += white.Bishop[1].cost;
+
+    if (white.Rook[0].piece_active)
+        total += white.Rook[0].cost;
+
+    if (white.Rook[1].piece_active)
+        total += white.Rook[1].cost;
+
+    for (int i = 0; i < size; i++)
+        if (white.Pawn[i].piece_active)
+            total += white.Pawn[i].cost;
+
+    // Black Pieces
+    if (black.King.piece_active)
+        total += black.King.cost;
+
+    if (black.Queen.piece_active)
+        total += black.Queen.cost;
+
+    if (black.Knight[0].piece_active)
+        total += black.Knight[0].cost;
+
+    if (black.Knight[1].piece_active)
+        total += black.Knight[1].cost;
+
+    if (black.Bishop[0].piece_active)
+        total += black.Bishop[0].cost;
+
+    if (black.Bishop[1].piece_active)
+        total += black.Bishop[1].cost;
+
+    if (black.Rook[0].piece_active)
+        total += black.Rook[0].cost;
+
+    if (black.Rook[1].piece_active)
+        total += black.Rook[1].cost;
+
+    for (int i = 0; i < size; i++)
+        if (black.Pawn[i].piece_active)
+            total += black.Pawn[i].cost;
+
     return total;
 }
 
-// todo
-void pawn_promotion() {}
+void pawn_promotion(piece *piece, int to_rank, int user_inp)
+{
+    /*
+    user_inp:
+    1 -> Q
+    2 -> N
+    3 -> B
+    4 -> R
+    */
+
+    if (piece->type != 'P' && piece->type != 'p')
+        return;
+    if (piece->color == 'W' && to_rank != size - 1)
+        return;
+    if (piece->color == 'B' && to_rank != 0)
+        return;
+
+    if (piece->color == 'W')
+    {
+        switch (user_inp)
+        {
+        case 1:
+            piece->type = 'Q';
+            piece->cost = 9;
+            piece->symbol = "♛";
+            break;
+        case 2:
+            piece->type = 'N';
+            piece->cost = 3;
+            piece->symbol = "♞";
+            break;
+        case 3:
+            piece->type = 'B';
+            piece->cost = 3;
+            piece->symbol = "♝";
+            break;
+        case 4:
+            piece->type = 'R';
+            piece->cost = 5;
+            piece->symbol = "♜";
+            break;
+        default:
+            printf("Invalid Input\n");
+            break;
+        }
+    }
+    else
+    {
+        switch (user_inp)
+        {
+        case 1:
+            piece->type = 'q';
+            piece->cost = -9;
+            piece->symbol = "♕";
+            break;
+        case 2:
+            piece->type = 'n';
+            piece->cost = -3;
+            piece->symbol = "♘";
+            break;
+        case 3:
+            piece->type = 'b';
+            piece->cost = -3;
+            piece->symbol = "♗";
+            break;
+        case 4:
+            piece->type = 'r';
+            piece->cost = -5;
+            piece->symbol = "♖";
+            break;
+        default:
+            printf("Invalid Input\n");
+            break;
+        }
+    }
+}
 
 int fileIndex(char file)
 {
@@ -213,6 +337,15 @@ void reset_board(square board[size][size])
     board[0][fileIndex('g')].piece = &white.Knight[1];
     board[0][fileIndex('h')].piece = &white.Rook[1];
 
+    board[0][fileIndex('a')].piece->piece_active = 1;
+    board[0][fileIndex('b')].piece->piece_active = 1;
+    board[0][fileIndex('c')].piece->piece_active = 1;
+    board[0][fileIndex('d')].piece->piece_active = 1;
+    board[0][fileIndex('e')].piece->piece_active = 1;
+    board[0][fileIndex('f')].piece->piece_active = 1;
+    board[0][fileIndex('g')].piece->piece_active = 1;
+    board[0][fileIndex('h')].piece->piece_active = 1;
+
     // Black pieces
     board[size - 1][fileIndex('a')].piece = &black.Rook[0];
     board[size - 1][fileIndex('b')].piece = &black.Knight[0];
@@ -223,16 +356,28 @@ void reset_board(square board[size][size])
     board[size - 1][fileIndex('g')].piece = &black.Knight[1];
     board[size - 1][fileIndex('h')].piece = &black.Rook[1];
 
+    board[size - 1][fileIndex('a')].piece->piece_active = 1;
+    board[size - 1][fileIndex('b')].piece->piece_active = 1;
+    board[size - 1][fileIndex('c')].piece->piece_active = 1;
+    board[size - 1][fileIndex('d')].piece->piece_active = 1;
+    board[size - 1][fileIndex('e')].piece->piece_active = 1;
+    board[size - 1][fileIndex('f')].piece->piece_active = 1;
+    board[size - 1][fileIndex('g')].piece->piece_active = 1;
+    board[size - 1][fileIndex('h')].piece->piece_active = 1;
+
     for (int i = 0; i < size; i++)
     {
         board[1][fileIndex('a' + i)].piece = &white.Pawn[i];
         white.Pawn[i].cost = 1;
         white.Pawn[i].symbol = "♟";
         white.Pawn[i].type = 'P';
+        white.Pawn[i].piece_active = 1;
+
         board[size - 2][fileIndex('a' + i)].piece = &black.Pawn[i];
         black.Pawn[i].cost = -1;
         black.Pawn[i].symbol = "♙";
         black.Pawn[i].type = 'p';
+        black.Pawn[i].piece_active = 1;
     }
 
     move_num = 1;
@@ -613,6 +758,9 @@ void piece_move(square board[size][size], char *move)
                 }
                 else
                     possible_en_passant_square = NULL;
+
+                if (valid_move && (to_rank == 0 || to_rank == size - 1))
+                    pawn_promotion(board[from_file][from_rank].piece, to_rank, 1);
             }
             else if (current_piece_type == 'N' || current_piece_type == 'n')
                 valid_move = valid_knight_move(board, from_rank, from_file, to_rank, to_file);
@@ -640,13 +788,7 @@ void piece_move(square board[size][size], char *move)
     }
     else if (valid_move == 2)
     {
-        for (int i = 0; i < size * 4; i++)
-            if (captured_pieces[i] == NULL)
-            {
-                captured_pieces[i] = board[to_rank][to_file].piece;
-                break;
-            }
-
+        board[to_rank][to_file].piece->piece_active = 0;
         board[to_rank][to_file].piece = board[from_rank][from_file].piece;
         board[from_rank][from_file].piece = NULL;
         move_num++;
@@ -655,22 +797,12 @@ void piece_move(square board[size][size], char *move)
     {
         if (to_rank - from_rank == 1)
         {
-            for (int i = 0; i < size * 4; i++)
-                if (captured_pieces[i] == NULL)
-                {
-                    captured_pieces[i] = board[to_rank - 1][to_file].piece;
-                    break;
-                }
+            board[to_rank - 1][to_file].piece->piece_active = 0;
             board[to_rank - 1][to_file].piece = NULL;
         }
         else if (to_rank - from_rank == -1)
         {
-            for (int i = 0; i < size * 4; i++)
-                if (captured_pieces[i] == NULL)
-                {
-                    captured_pieces[i] = board[to_rank + 1][to_file].piece;
-                    break;
-                }
+            board[to_rank + 1][to_file].piece->piece_active = 0;
             board[to_rank + 1][to_file].piece = NULL;
         }
         else
@@ -725,3 +857,5 @@ void main()
         piece_move(board, &usr_inp);
     }
 }
+
+// Sample Game: e2e4 e7e5 g1f3 g8f6 f3e5 d7d6 e5f3 f6e4 d1e2
